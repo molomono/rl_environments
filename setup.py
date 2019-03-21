83963bf590ca487f0b110750aab6fbadbc1b6124
molomono <molomono@users.noreply.github.com>
2019-03-21 15:17:43 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,5 @@ sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'rl_environments'))
 setup(name='rl_environments',
       packages=['rl_environments'],
       version='0.0.1',
-      install_requires=['gym',
-                        'pybullet',
-						'rl_coach']
+      install_requires=['pybullet']
 )
