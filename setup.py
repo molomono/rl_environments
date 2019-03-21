f83e2d1b4c6d038f0d935666641416b4d01b27e0
molomono <molomono@users.noreply.github.com>
2019-03-21 14:44:19 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ import sys, os.path
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'rl_environments'))
 
 setup(name='rl_environments',
+      packages=['rl_environments']  
       version='0.0.1',
       install_requires=['gym',
                         'pybullet',
