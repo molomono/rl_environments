0651f1aa9f9d25e96da5beff489909c4630ca45f
molomono <molomono@users.noreply.github.com>
2019-03-25 15:31:38 +0100

    Update balancebot_vrep_env.py

--- rl_environments/vrep/balancebot_vrep_env.py
+++ rl_environments/vrep/balancebot_vrep_env.py
@@ -160,8 +160,9 @@ class BalanceBotVrepEnv(vrep_env.VrepEnv):
 		#for certain types of RL such as DRL this can always be done
 		#and can improve convergence properties
 		a = 0.1
-		b = -100.0
-		reward = a*(5.0*(r_alive) + 0.75*r_regul) #+ (1.0)* gaussian_2d(head_pos_x, head_pos_y) + (1.0)*theta
+		b = 0.0
+		reward = a*(5.0*(r_alive) + 0.75*r_regul) + b 
+		#+ (1.0)* gaussian_2d(head_pos_x, head_pos_y) + (1.0)*theta
 		
 		#Check if the balancebot fell over 
 		angle_base = self.obj_get_orientation(self.oh_shape[0])
