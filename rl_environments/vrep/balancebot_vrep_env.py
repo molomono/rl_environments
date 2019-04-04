2dc64527b6fd7e1764ed23c21a29f379d6bc3795
molomono <molomono@users.noreply.github.com>
2019-04-04 13:44:55 +0200

    Update balancebot_vrep_env.py

--- rl_environments/vrep/balancebot_vrep_env.py
+++ rl_environments/vrep/balancebot_vrep_env.py
@@ -149,7 +149,7 @@ class BalanceBotVrepEnv(vrep_env.VrepEnv):
 		head_pos_y = self.observation[1] # left/right
 		theta  	= gaussian( self.observation[2], sig=1.5 ) 
 		r_regul = gaussian( action, sig=0.707)
-		r_alive = 1.0
+		r_alive = 2.0
 		# example: different weights in reward 
 		#attempts to stay alive and stay centered
 		
@@ -161,7 +161,7 @@ class BalanceBotVrepEnv(vrep_env.VrepEnv):
 		a = 1
 		b = -0.5
 		#reward = a*(5.0*(r_alive) + 0.75*r_regul) + b 
-		reward = a*(gaussian_2d(head_pos_x, head_pos_y) * (0.5*r_regul + 0.5)) + b
+		reward = a*(r_alive + gaussian_2d(head_pos_x, head_pos_y) * (0.5*r_regul + 0.5)) + b
 		#+ (1.0)* gaussian_2d(head_pos_x, head_pos_y) + (1.0)*theta
 		
 		#Check if the balancebot fell over 
