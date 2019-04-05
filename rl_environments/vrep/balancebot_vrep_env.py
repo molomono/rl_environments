8e2ac0693263917858564b2a2492a83e2aca5f52
molomono <molomono@users.noreply.github.com>
2019-04-05 15:07:24 +0200

    Update balancebot_vrep_env.py

--- rl_environments/vrep/balancebot_vrep_env.py
+++ rl_environments/vrep/balancebot_vrep_env.py
@@ -165,7 +165,8 @@ class BalanceBotVrepEnv(vrep_env.VrepEnv):
 
 		#TODO: change the action to the deltaPos of the wheels:
 		delta_pos = np.asarray([self.l_wheel_delta, self.r_wheel_delta])
-		r_regul = gaussian( delta_pos, sig=0.01)
+		print(delta_pos)
+		r_regul = gaussian( delta_pos, sig=0.001)
 		r_alive = 2.0
 		# example: different weights in reward 
 		#attempts to stay alive and stay centered
