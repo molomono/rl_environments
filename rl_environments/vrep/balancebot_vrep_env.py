71286b310b2f89794f49c29f126b9dcaac367a7c
molomono <molomono@users.noreply.github.com>
2019-04-10 13:34:48 +0200

    Update balancebot_vrep_env.py

--- rl_environments/vrep/balancebot_vrep_env.py
+++ rl_environments/vrep/balancebot_vrep_env.py
@@ -182,7 +182,7 @@ class BalanceBotVrepEnv(vrep_env.VrepEnv):
 		a = 1.0
 		b = -1.0		
 		#reward = 1.0
-		reward = (a*(8.0*(r_alive) + 0.1*r_regul) + b) - 8
+		reward = (a*(8.0*(r_alive) + 0.1*r_regul) + b) - 7.0
 		#reward = r_regul
 		#TODO: The reward function punishes high action, however action is torque, THIS IS FIXED NOW
 		# This seems to be bad because a change of velocity is what we want to control, 
