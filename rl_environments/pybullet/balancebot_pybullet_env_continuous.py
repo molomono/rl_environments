1e175b41fc7f982b8dfe7847633d887a10a7db4d
molomono <molomono@users.noreply.github.com>
2019-04-26 12:10:33 +0200

    Update balancebot_pybullet_env_continuous.py

--- rl_environments/pybullet/balancebot_pybullet_env_continuous.py
+++ rl_environments/pybullet/balancebot_pybullet_env_continuous.py
@@ -112,8 +112,8 @@ class BalanceBotPyBulletEnvContinuous(gym.Env):
         path = "C:/github/rl_environments/rl_environments/pybullet/"
         self.botId = p.loadURDF(path + "balancebot_simple.urdf",
                            cubeStartPos,
-                           cubeStartOrientation) + 1
-                           
+                           cubeStartOrientation)
+
         # you *have* to compute and return the observation from reset()
         self._observation = self._compute_observation()
         return np.array(self._observation)
