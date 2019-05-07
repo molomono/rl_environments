b26d178667a09e8bbf5836be8d3a2301d82e66e3
molomono <molomono@users.noreply.github.com>
2019-05-07 14:39:39 +0200

    Update balancebot_vrep_env_noise.py

--- rl_environments/vrep/balancebot_vrep_env_noise.py
+++ rl_environments/vrep/balancebot_vrep_env_noise.py
@@ -186,8 +186,8 @@ class BalanceBotVrepEnvNoise(vrep_env.VrepEnv):
 		delta_pos = np.asarray([self.l_wheel_delta, self.r_wheel_delta])
 		#print(delta_pos)
 		r_regul = gaussian( 20* delta_pos, sig=1.0)
-		r_ang_x_en = gaussian(np.mean(np.abs(self.observation[3])**2), sig=1.4)  #kinetic energy
-		r_ang_y_en = gaussian(np.mean(np.abs(self.observation[4])**2), sig=1.4)
+		#r_ang_x_en = gaussian(np.mean(np.abs(self.observation[3])**2), sig=1.4)  #kinetic energy
+		#r_ang_y_en = gaussian(np.mean(np.abs(self.observation[4])**2), sig=1.4)
 		r_ang_z_en = gaussian(30* np.abs(1/2 * self.observation[5]**2))  #kinetic energy
 		#print(r_ang_xy_en, self.observation[3:5])
 		r_alive = 1.0
@@ -207,7 +207,7 @@ class BalanceBotVrepEnvNoise(vrep_env.VrepEnv):
 
 		##
 		a = 1./11.
-		reward = (7.*r_alive + r_regul + r_ang_x_en + r_ang_y_en + r_ang_z_en) * a
+		reward = (8.*r_alive + r_regul + r_ang_z_en) * a
 		
 		#reward = (a*(8.0*(r_alive) + 0.1*r_regul) + b) - 7.0
 		#reward = r_regul
