640f833ef6f1d076cf6b9aa2338d9a80f8c193f8
molomono <molomono@users.noreply.github.com>
2019-05-22 11:55:54 +0200

    Update balancebot_vrep_env_noise.py

--- rl_environments/vrep/balancebot_vrep_env_noise.py
+++ rl_environments/vrep/balancebot_vrep_env_noise.py
@@ -209,8 +209,8 @@ class BalanceBotVrepEnvNoise(vrep_env.VrepEnv, SensorInfo):
 		if VECTOR_ACTION:
 			print("original action: ", action)
 			kinematics = np.matrix([[-1, 1], [1, 1]])
-			normalize_action = lambda x: np.asarray(x * 1./np.linalg.norm(x) * self.joints_max_velocity)
-			action = np.array(normalize_action(np.matrix(action) * kinematics)).reshape(-1)
+			#normalize_action = lambda x: np.asarray( ( x * 1./np.linalg.norm(x) * self.joints_max_velocity if np.linalg.norm(x) < )  )
+			action = (np.matrix(action) * kinematics).reshape(-1)
 			print("New Action: ", action)
 
 		# #modify Either clip the actions outside the space or assert the space contains them
