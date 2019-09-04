e0e264375f6983667dea656d15818cadd00f3e03
Brian Armstrong <BAR@demcon.local>
2019-09-04 16:49:42 +0200

    Update balancebot_vrep_rotation.py

--- rl_environments/vrep/balancebot_vrep_rotation.py
+++ rl_environments/vrep/balancebot_vrep_rotation.py
@@ -43,12 +43,12 @@ class BalanceBotVrepEnvRotation(BalanceBotVrepEnvNoise):
 		rel_pos_dist = np.array([self.goal[0]-self.observation[9], self.goal[1]-self.observation[10]])
 		# Calculate the Angle of the goal with respect to the Y axis of the robot.
 		# First calculate the angle of the goal with respect to the inertial Y axis
-		goal_angle = np.arctan2(rel_pos_dist[1],rel_pos_dist[0])
+		goal_angle = np.arctan2(rel_pos_dist[0], rel_pos_dist[1])
 		# Retrieve the angle between the robot and the inertial X axis
 		robot_angle = np.arctan2(self.observation[8], self.observation[7])
 
 		# Ranges from 0 to 1 rewarding alligning the Y axis of the robot with the goal.
-		dense_reward = np.abs((np.sin(goal_angle) * np.sin(robot_angle)))
+		dense_reward = np.abs((np.cos(goal_angle) * np.cos(robot_angle)))
 
 		print("AngleRobot", goal_angle )
 		print("AngleGoal", robot_angle )
