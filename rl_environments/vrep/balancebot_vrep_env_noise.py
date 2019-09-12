0bb48b1d4f7f6d29d27c91f24d97d1e6d1bf9864
Brian Armstrong <BAR@demcon.local>
2019-09-13 00:15:01 +0200

    Update balancebot_vrep_env_noise.py

--- rl_environments/vrep/balancebot_vrep_env_noise.py
+++ rl_environments/vrep/balancebot_vrep_env_noise.py
@@ -306,15 +306,16 @@ class BalanceBotVrepEnvNoise(vrep_env.VrepEnv):
 		start_pitch = np.random.uniform(-np.pi/18, np.pi/18)
 		start_theta = np.random.uniform(-np.pi, np.pi)
 
-		start_x = np.random.uniform(-3., 3.)
-		start_y = np.random.uniform(-3., 3.)
+		start_x = np.random.uniform(-3., 3.) * 0.0
+		start_y = np.random.uniform(-3., 3.) * 0.0
 
 		self.obj_set_position(handle=self.oh_shape[0], pos=np.array([start_x, start_y, 0.15]))
 		self.obj_set_orientation(handle=self.oh_shape[0], eulerAngles=np.array([start_pitch, 0.0, start_theta]))
 	
 		#Sample an initial goal
 		self.goal = self.sample_goal()
-		self.goal = np.array([self.goal[1] * np.sin(start_theta) + self.goal[0] * np.cos(start_theta) + start_x, self.goal[1] * np.cos(start_theta) + self.goal[0] * np.sin(start_theta) + start_y])
+		self.goal = np.array([	self.goal[1] * np.sin(start_theta) + self.goal[0] * np.cos(start_theta) + start_x, \
+								self.goal[1] * np.cos(start_theta) + self.goal[0] * np.sin(start_theta) + start_y])
 		print("Goal: ", self.goal)
 
 		#Make an initial observation - used to take the first action
