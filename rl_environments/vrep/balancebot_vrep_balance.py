5b76b7408dc90c4db5b7754370e83e27bf3214f5
Brian Armstrong <BAR@demcon.local>
2019-09-13 15:21:02 +0200

    Update balancebot_vrep_balance.py

--- rl_environments/vrep/balancebot_vrep_balance.py
+++ rl_environments/vrep/balancebot_vrep_balance.py
@@ -20,8 +20,9 @@ class BalanceBotVrepEnvBalance(BalanceBotVrepEnvNoise):
 			self.goal = self.sample_goal()
 			theta_continuous = self.observation[7:9]
 			current_position = self.observation[9:11]	
-			self.goal = np.array([	self.goal[1] * theta_continuous[0] + self.goal[0] * theta_continuous[1] + current_position[0], \
-									self.goal[1] * theta_continuous[1] + self.goal[0] * theta_continuous[0] + current_position[1]])
+			theta = np.arctan2(theta_continuous[1],theta_continuous[0]) + np.pi/2
+			self.goal = np.array([	self.goal[1] * np.sin(theta) + self.goal[0] * np.cos(theta) + current_position[0], \
+									self.goal[1] * np.cos(theta) + self.goal[0] * np.sin(theta) + current_position[1]])
 
 		return dense_reward + sparse_reward
 
