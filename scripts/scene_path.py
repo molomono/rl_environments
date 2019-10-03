dafd210a327919a6d48d9dc4b4a20fe4583faa93
Brian Armstrong <BAR@demcon.local>
2019-10-03 11:42:29 +0200

    Update scene_path.py

--- scripts/scene_path.py
+++ scripts/scene_path.py
@@ -1,1 +1,6 @@
-vrep_scenes_path = 'C:/Program Files/V-REP3/V-REP_PRO/scenes'
+import os
+
+if os.name == 'nt':
+	vrep_scenes_path = 'C:/Program Files/V-REP3/V-REP_PRO/scenes'
+else:
+	vrep_scenes_path = os.environ['VREP_SCENES_PATH']
