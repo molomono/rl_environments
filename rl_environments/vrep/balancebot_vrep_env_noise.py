dd2f25d75f96d4414bdc7af1be278b6602a80492
Brian Armstrong <BAR@demcon.local>
2019-10-03 13:48:08 +0200

    Update balancebot_vrep_env_noise.py

--- rl_environments/vrep/balancebot_vrep_env_noise.py
+++ rl_environments/vrep/balancebot_vrep_env_noise.py
@@ -303,7 +303,7 @@ class BalanceBotVrepEnvNoise(vrep_env.VrepEnv):
 		start_y = np.random.uniform(-3., 3.) 
 
 		#Rotate
-		self.obj_set_orientation(handle=self.oh_shape[0], eulerAngles=np.array([0.0, start_pitch, -start_theta]), relative_to=self.oh_shape[0])
+		self.obj_set_orientation(handle=self.oh_shape[0], eulerAngles=np.array([0.0, 0.0, -start_theta]), relative_to=self.oh_shape[0])
 		#Translate
 		self.obj_set_position(handle=self.oh_shape[0], pos=np.array([start_x, start_y, 0.15]))
 
